063abee106e2bd3f7e7e3843ba2a565d5e92dfb0
Donald Thompson <witt3rd@witt3rd.com>
2024-05-29 07:31:35 -0700

    full DSPy examples from docs

---
+++ posts/dspy/groq_lm.py
@@ -0,0 +1,65 @@
+import os
+
+from dsp import LM
+from groq import Groq as GroqClient
+
+import dspy
+
+LLAMA3_MODEL = "llama3-70b-8192"
+
+
+class GroqLM(LM):
+    def __init__(
+        self, model=LLAMA3_MODEL, **kwargs
+    ):  # model="mixtral-8x7b-32768", **kwargs):
+        # TODO - check of passed model is in list of Groq - if not set to some Groq default
+        # model="llama3-70b-8192" # this is a fix cs somewhere the the model getting still set to openai gpt-3.5-turbo-instruct
+        super().__init__(model)
+
+        print("Groq model used today: " + model)
+        self.provider = "default"
+        self.history = []
+        groq_api_key = os.environ.get("GROQ_API_KEY")
+
+        if groq_api_key is None:
+            raise ValueError("GROQ_API_KEY environment variable not found")
+
+        self.client = GroqClient(api_key=os.environ.get("GROQ_API_KEY"))
+
+    def basic_request(self, prompt, **kwargs):
+        pass
+
+    def __call__(self, prompt, only_completed=True, return_sorted=False, **kwargs):
+        chat_completion = self.client.chat.completions.create(
+            messages=[
+                {
+                    "role": "user",
+                    "content": prompt,
+                },
+            ],
+            **self.kwargs,
+        )
+        return [chat_completion.choices[0].message.content]
+
+
+def init_dspy(
+    lm_class: LM = dspy.OpenAI,
+    max_tokens: int = 800,
+    model: str = "gpt-3.5-turbo-instruct",
+    lm_instance=None,
+):
+    if lm_instance:
+        dspy.settings.configure(lm=lm_instance)
+        return lm_instance
+    lm = lm_class(max_tokens=max_tokens, model=model)
+    dspy.settings.configure(lm=lm)
+    return lm
+
+
+if __name__ == "__main__":
+    init_dspy(GroqLM, model=LLAMA3_MODEL, max_tokens=2000)
+    # init_dspy(max_tokens=2000)
+    pred = dspy.Predict("prompt -> code")(
+        prompt="Fast API CRUD endpoint for fire alarm global IoT network"
+    )
+    print(pred.code)
